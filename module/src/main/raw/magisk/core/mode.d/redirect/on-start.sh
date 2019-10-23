a21ea99f173951981644200c42b13a0177611e0e
h_cheung <h_cheung@outlook.com>
2019-10-23 20:59:30 +0800

    add ipv6 redir scripts

--- module/src/main/raw/magisk/core/mode.d/redirect/on-start.sh
+++ module/src/main/raw/magisk/core/mode.d/redirect/on-start.sh
@@ -13,13 +13,6 @@
 # - CLASH_GID: clash running gid
 #
 
-assert() {
-    "$@"
-    if [[ $? != 0 ]];then
-        echo "Command $@ failure"
-        exit 1
-    fi
-}
 
 if [[ "${CLASH_UID}" = "" ]];then
     echo "Clash uid not found"
@@ -31,48 +24,79 @@ if [[ "${CLASH_REDIR_PORT}" = "" ]];then
     exit 1
 fi
 
-assert iptables -t nat -N CLASH_LOCAL
-assert iptables -t nat -N CLASH_EXTERNAL
+iptables -t nat -N CLASH_LOCAL
+iptables -t nat -N CLASH_EXTERNAL
 
-assert iptables -t nat -A CLASH_LOCAL -m owner --uid-owner ${CLASH_UID} -j RETURN
-assert iptables -t nat -A CLASH_LOCAL -d 0.0.0.0/8 -j RETURN
-assert iptables -t nat -A CLASH_LOCAL -d 127.0.0.0/8 -j RETURN
-assert iptables -t nat -A CLASH_LOCAL -d 224.0.0.0/4 -j RETURN
-assert iptables -t nat -A CLASH_LOCAL -d 172.16.0.0/12 -j RETURN
-assert iptables -t nat -A CLASH_LOCAL -d 127.0.0.0/8 -j RETURN
-assert iptables -t nat -A CLASH_LOCAL -d 169.254.0.0/16 -j RETURN
-assert iptables -t nat -A CLASH_LOCAL -d 240.0.0.0/4 -j RETURN
-assert iptables -t nat -A CLASH_LOCAL -d 192.168.0.0/16 -j RETURN
-assert iptables -t nat -A CLASH_LOCAL -d 10.0.0.0/8 -j RETURN
-assert iptables -t nat -A CLASH_LOCAL -p tcp -j REDIRECT --to-ports ${CLASH_REDIR_PORT}
+iptables -t nat -A CLASH_LOCAL -m owner --uid-owner ${CLASH_UID} -j RETURN
+iptables -t nat -A CLASH_LOCAL -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A CLASH_LOCAL -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A CLASH_LOCAL -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A CLASH_LOCAL -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A CLASH_LOCAL -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A CLASH_LOCAL -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A CLASH_LOCAL -d 240.0.0.0/4 -j RETURN
+iptables -t nat -A CLASH_LOCAL -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A CLASH_LOCAL -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A CLASH_LOCAL -p tcp -j REDIRECT --to-ports ${CLASH_REDIR_PORT}
 
-assert iptables -t nat -A CLASH_EXTERNAL -d 0.0.0.0/8 -j RETURN
-assert iptables -t nat -A CLASH_EXTERNAL -d 127.0.0.0/8 -j RETURN
-assert iptables -t nat -A CLASH_EXTERNAL -d 224.0.0.0/4 -j RETURN
-assert iptables -t nat -A CLASH_EXTERNAL -d 172.16.0.0/12 -j RETURN
-assert iptables -t nat -A CLASH_EXTERNAL -d 127.0.0.0/8 -j RETURN
-assert iptables -t nat -A CLASH_EXTERNAL -d 169.254.0.0/16 -j RETURN
-assert iptables -t nat -A CLASH_EXTERNAL -d 240.0.0.0/4 -j RETURN
-assert iptables -t nat -A CLASH_EXTERNAL -d 192.168.0.0/16 -j RETURN
-assert iptables -t nat -A CLASH_EXTERNAL -d 10.0.0.0/8 -j RETURN
-assert iptables -t nat -A CLASH_EXTERNAL -p tcp -j REDIRECT --to-ports ${CLASH_REDIR_PORT}
+iptables -t nat -A CLASH_EXTERNAL -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A CLASH_EXTERNAL -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A CLASH_EXTERNAL -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A CLASH_EXTERNAL -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A CLASH_EXTERNAL -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A CLASH_EXTERNAL -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A CLASH_EXTERNAL -d 240.0.0.0/4 -j RETURN
+iptables -t nat -A CLASH_EXTERNAL -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A CLASH_EXTERNAL -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A CLASH_EXTERNAL -p tcp -j REDIRECT --to-ports ${CLASH_REDIR_PORT}
 
-assert iptables -t nat -I OUTPUT -p tcp -j CLASH_LOCAL
-assert iptables -t nat -I PREROUTING -p tcp -j CLASH_EXTERNAL
+iptables -t nat -I OUTPUT -p tcp -j CLASH_LOCAL
+iptables -t nat -I PREROUTING -p tcp -j CLASH_EXTERNAL
+
+ip6tables -t nat -N CLASH_LOCAL6
+ip6tables -t nat -N CLASH_EXTERNAL6
+ip6tables -t nat -A CLASH_LOCAL6 -m owner --uid-owner ${CLASH_UID} -j RETURN
+ip6tables -t nat -A CLASH_LOCAL6 -d ::/128 -j RETURN
+ip6tables -t nat -A CLASH_LOCAL6 -d ::1/128 -j RETURN
+ip6tables -t nat -A CLASH_LOCAL6 -d ff00::/8 -j RETURN
+ip6tables -t nat -A CLASH_LOCAL6 -d fe80::/10 -j RETURN
+ip6tables -t nat -A CLASH_LOCAL6 -p tcp -j REDIRECT --to-ports ${CLASH_REDIR_PORT}
+
+ip6tables -t nat -A CLASH_EXTERNAL6 -d ::/128 -j RETURN
+ip6tables -t nat -A CLASH_EXTERNAL6 -d ::1/128 -j RETURN
+ip6tables -t nat -A CLASH_EXTERNAL6 -d ff00::/8 -j RETURN
+ip6tables -t nat -A CLASH_EXTERNAL6 -d fe80::/10 -j RETURN
+ip6tables -t nat -A CLASH_EXTERNAL6 -p tcp -j REDIRECT --to-ports ${CLASH_REDIR_PORT}
+
+ip6tables -t nat -I OUTPUT -p tcp -j CLASH_LOCAL6
+ip6tables -t nat -I PREROUTING -p tcp -j CLASH_EXTERNAL6
 
 if [[ "${CLASH_DNS_PORT}" = "" ]];then
     exit 0
 fi
 
-assert iptables -t nat -N CLASH_DNS_LOCAL
-assert iptables -t nat -N CLASH_DNS_EXTERNAL
+iptables -t nat -N CLASH_DNS_LOCAL
+iptables -t nat -N CLASH_DNS_EXTERNAL
 
-assert iptables -t nat -A CLASH_DNS_LOCAL -p udp ! --dport 53 -j RETURN
-assert iptables -t nat -A CLASH_DNS_LOCAL -m owner --uid-owner ${CLASH_UID} -j RETURN
-assert iptables -t nat -A CLASH_DNS_LOCAL -p udp -j REDIRECT --to-ports ${CLASH_DNS_PORT}
+iptables -t nat -A CLASH_DNS_LOCAL -p udp ! --dport 53 -j RETURN
+iptables -t nat -A CLASH_DNS_LOCAL -m owner --uid-owner ${CLASH_UID} -j RETURN
+iptables -t nat -A CLASH_DNS_LOCAL -p udp -j REDIRECT --to-ports ${CLASH_DNS_PORT}
 
-assert iptables -t nat -A CLASH_DNS_EXTERNAL -p udp ! --dport 53 -j RETURN
-assert iptables -t nat -A CLASH_DNS_EXTERNAL -p udp -j REDIRECT --to-ports ${CLASH_DNS_PORT}
+iptables -t nat -A CLASH_DNS_EXTERNAL -p udp ! --dport 53 -j RETURN
+iptables -t nat -A CLASH_DNS_EXTERNAL -p udp -j REDIRECT --to-ports ${CLASH_DNS_PORT}
 
-assert iptables -t nat -I OUTPUT -p udp -j CLASH_DNS_LOCAL
-assert iptables -t nat -I PREROUTING -p udp -j CLASH_DNS_EXTERNAL
+iptables -t nat -I OUTPUT -p udp -j CLASH_DNS_LOCAL
+iptables -t nat -I PREROUTING -p udp -j CLASH_DNS_EXTERNAL
+
+ip6tables -t nat -N CLASH_DNS_LOCAL6
+ip6tables -t nat -N CLASH_DNS_EXTERNAL6
+
+ip6tables -t nat -A CLASH_DNS_LOCAL6 -p udp ! --dport 53 -j RETURN
+ip6tables -t nat -A CLASH_DNS_LOCAL6 -m owner --uid-owner ${CLASH_UID} -j RETURN
+ip6tables -t nat -A CLASH_DNS_LOCAL6 -p udp -j REDIRECT --to-ports ${CLASH_DNS_PORT}
+
+ip6tables -t nat -A CLASH_DNS_EXTERNAL6 -p udp ! --dport 53 -j RETURN
+ip6tables -t nat -A CLASH_DNS_EXTERNAL6 -p udp -j REDIRECT --to-ports ${CLASH_DNS_PORT}
+
+ip6tables -t nat -I OUTPUT -p udp -j CLASH_DNS_LOCAL6
+ip6tables -t nat -I PREROUTING -p udp -j CLASH_DNS_EXTERNAL6
--- module/src/main/raw/magisk/core/mode.d/redirect/on-stop.sh
+++ module/src/main/raw/magisk/core/mode.d/redirect/on-stop.sh
@@ -13,13 +13,6 @@
 # - CLASH_GID: clash running gid
 #
 
-assert() {
-    "$@"
-    if [[ $? != 0 ]];then
-        echo "Command $@ failure"
-        exit 1
-    fi
-}
 
 if [[ "${CLASH_REDIR_PORT}" = "" ]];then
     echo "Clash redir disabled"
@@ -35,6 +28,15 @@ iptables -t nat -X CLASH_LOCAL
 iptables -t nat -F CLASH_EXTERNAL
 iptables -t nat -X CLASH_EXTERNAL
 
+ip6tables -t nat -D OUTPUT -p tcp -j CLASH_LOCAL6
+ip6tables -t nat -D PREROUTING -p tcp -j CLASH_EXTERNAL6
+
+ip6tables -t nat -F CLASH_LOCAL6
+ip6tables -t nat -X CLASH_LOCAL6
+
+ip6tables -t nat -F CLASH_EXTERNAL6
+ip6tables -t nat -X CLASH_EXTERNAL6
+
 if [[ "${CLASH_DNS_PORT}" = "" ]];then
     exit 0
 fi
@@ -47,3 +49,12 @@ iptables -t nat -X CLASH_DNS_LOCAL
 
 iptables -t nat -F CLASH_DNS_EXTERNAL
 iptables -t nat -X CLASH_DNS_EXTERNAL
+
+ip6tables -t nat -D OUTPUT -p udp -j CLASH_DNS_LOCAL6
+ip6tables -t nat -D PREROUTING -p udp -j CLASH_DNS_EXTERNAL6
+
+ip6tables -t nat -F CLASH_DNS_LOCAL6
+ip6tables -t nat -X CLASH_DNS_LOCAL6
+
+ip6tables -t nat -F CLASH_DNS_EXTERNAL6
+ip6tables -t nat -X CLASH_DNS_EXTERNAL6
